0c6bcbfa498e54b575b58640d4d4709d0005368b
engabdullah-2024 <enga95311@gmail.com>
2024-09-30 22:29:32 +0300

    Result Updated for calculator page

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -43,7 +43,7 @@ const Calculator = () => {
         <Header/>
 
     <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">Age Master</h2>
+      <h2 className="text-2xl font-bold mb-4">Calculate Your Age</h2>
       <div className="flex items-center">
         <FaCalendarAlt className="mr-2" />
         <input 
