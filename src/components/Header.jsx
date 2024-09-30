fba816bd2e1e88e028457cb18830c4ddd489bc8e
engabdullah-2024 <enga95311@gmail.com>
2024-09-30 22:22:31 +0300

    Result Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <nav className="flex justify-between">
-        <h1 className="text-2xl font-bold">Age Master</h1>
+      <Link to="/"> <h1 className="text-2xl font-bold">Age Master</h1></Link> 
         <div>
           <Link to="/" className="mr-4">Home</Link>
           <Link to="/about" className="mr-4">About</Link>
